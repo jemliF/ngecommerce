d80f1c751bd505d9e626c63c6d9e7e6ca2971629
jemliF <jemlifathi2013@gmail.com>
2016-12-05 02:22:49 +0100

    adding navbar component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavbarComponent} from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/app.component.html',
+  components: [NavbarComponent]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  name = 'Angular';
+}
